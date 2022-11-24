47c8a9d7b387a123cc15ec90faf53dccb74b38fc
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-11-24 14:21:37 +0200

    selectors

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -8,32 +8,35 @@ import {
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContacts } from 'redux/operations';
-import { getFilter, getContacts,getLoading,getError  } from '../../redux/selectors';
+import {
+  getLoading,
+  getError,
+  selectVisibleContacts,
+} from '../../redux/selectors';
 import { fetchContacts } from '../../redux/operations';
 
 export const ContactsList = () => {
-  const contacts = useSelector(getContacts);
-  const isLoading = useSelector(getLoading);  
+  const contacts = useSelector(selectVisibleContacts);
+  const isLoading = useSelector(getLoading);
   const error = useSelector(getError);
 
-  const filter = useSelector(getFilter);
+  //   const filter = useSelector(getFilter);
   const dispatch = useDispatch();
-  
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  const visibleContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase().trim())
-  );
+  // Перенесення логіки до селекторів
+  // const visibleContacts = contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase().trim())
+  //   );
 
   return (
     <Container>
-
       <h2>My contacts</h2>
       {isLoading && !error && <b>Request in progress...</b>}
-      {visibleContacts.map(contact => (
+      {contacts.map(contact => (
         <ListOfContact key={contact.id}>
           <ContactItem>
             {contact.name}: {contact.phone}{' '}
